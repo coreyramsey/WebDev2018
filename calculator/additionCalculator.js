82ece8c034dbbbad5a65dca7e26b7c44c0e79850
Corey Ramsey <coreyramsey@gmail.com>
2018-05-29 22:38:56 -0500

    added calculator

---
+++ calculator/additionCalculator.js
@@ -0,0 +1,6 @@
+var firstNumber = prompt("Please enter first number to add");
+var secondNumber = prompt("Please enter second number to add");
+var sum = Number(firstNumber) + Number(secondNumber);
+alert("Your total is " + sum);
+
+
