897e020add6a1d5b69b2452111b793d878d59e0a
Corey Ramsey <coreyramsey@gmail.com>
2018-07-08 23:10:26 -0500

    added js for adding/removing buttons

--- DOMselector/script.js
+++ DOMselector/script.js
@@ -1,6 +1,34 @@
 var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
+var deleteBtns = document.getElementsByClassName("delete");
+var items = ul.getElementsByTagName("li");
+
+//add event listener to first 6 btns in HTML file
+for(var i = 0; i < deleteBtns.length; i++){
+	deleteBtns[i].addEventListener("click", removeParent, false);
+}
+
+
+//from StackOverflow:
+function removeParent(evt) {
+  evt.target.removeEventListener("click", removeParent, false);
+  evt.target.parentNode.remove();
+}
+
+//click on a list item and it strikethroughs the text
+function getEventTarget(e){
+	e = e || window.event;
+	return e.target || e.srcElement;
+}
+
+ul.onclick = function(event){
+	var target = getEventTarget(event);
+	target.classList.toggle("done");
+}
+
+
+//adding new items:
 
 function inputLength() {
 	return input.value.length;
